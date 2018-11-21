6e0a28181578b314877c60d46f4969f8cba2c2b0
Claudia Fox <claudiafox95@icloud.com>
2018-11-21 14:12:33 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,14 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  @@brand_count = 0 
   
   BRANDS = []
 
   def initialize(brand)
     @brand = brand
     BRANDS << brand
+    @@brand_count = @@brand_count + 1
     
   end
 
